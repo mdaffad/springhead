132bac7022672af58f1fad0fbbd01e8c36772bd4
Muhammad Daffa Dinaya <13518141@std.stei.itb.ac.id>
2022-10-22 18:50:52 +0700

    chore: remove unused import

--- springhead/models/__init__.py
+++ springhead/models/__init__.py
@@ -2,4 +2,3 @@ from .model_loader import ModelLoader
 from .pipeline import Pipeline
 from .process import Process, ProcessType
 from .specification import Specification
-from .vectorization_process import VectorizationProcess
