b3a80ef186016387a3819807d4bddebaddc322e5
Muhammad Daffa Dinaya <13518141@std.stei.itb.ac.id>
2022-11-06 16:29:23 +0700

    feat: add clustream with pickle

--- springhead/service_layer/handlers/clustering.py
+++ springhead/service_layer/handlers/clustering.py
@@ -1,8 +1,14 @@
+from river.cluster import CluStream
 from statefun import Context, Message
 
 from springhead.models import Process
 
 
 def clustream(context: Context, message: Message, process: Process) -> None:
-    # TODO: clustream
+    clustream = context.storage.clustream or None
+    if not clustream:
+        clustream = clustream = CluStream(
+            n_macro_clusters=3, max_micro_clusters=5, time_gap=3, seed=0, halflife=0.4
+        )
+    print(clustream)
     pass
