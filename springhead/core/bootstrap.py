6ec35403ccf404678823186465e5853f60217326
Muhammad Daffa Dinaya <13518141@std.stei.itb.ac.id>
2023-01-04 09:12:46 +0700

    feat: add custom process

--- springhead/core/bootstrap.py
+++ springhead/core/bootstrap.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Callable, Dict, List
 
-from statefun import Context, Message, RequestReplyHandler
+from statefun import Context, Message, RequestReplyHandler, ValueSpec
 
 from springhead.models import Pipeline, Process
 from springhead.service_layer import (
@@ -20,7 +20,8 @@ class Bootstrap:
 
 
 async def bootstrap(
-    custom_function: Dict[str, Callable[[Context, Message, Process], None]] = dict()
+    custom_functions: Dict[str, Callable[[Context, Message, Process], None]] = dict(),
+    custom_value_specs: Dict[str, List[ValueSpec]] = dict(),
 ) -> Bootstrap:
     specification_builder = SpecificationBuilder(
         "./app/specifications.yml"
@@ -28,7 +29,13 @@ async def bootstrap(
     specifications = specification_builder.read_spec_file()
     processes: List[Process] = []
     for specification in specifications:
-        process = ProcessBuilder.build(specification=specification)
+        custom_function = custom_functions.get(specification.typename)
+        custom_value_spec = custom_value_specs.get(specification.typename, [])
+        process = ProcessBuilder.build(
+            specification=specification,
+            custom_function=custom_function,
+            custom_value_spec=custom_value_spec,
+        )
         processes.append(process)
 
     pipeline: Pipeline = PipelineBuilder.build(processes=processes)
